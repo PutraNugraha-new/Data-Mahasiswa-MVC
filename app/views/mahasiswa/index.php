40b86e9ec2636aef25e6a8cc969e173093c0f366
PutraNugraha-New <putranugraha803@gmail.com>
2022-07-24 11:50:06 +0700

    +searching - selesai

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -4,11 +4,27 @@
             <?php Flasher::flash(); ?>
         </div>
     </div>
-    <div class="row">
+
+    <div class="row mb-3">
         <div class="col-lg-6">
             <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
-                Tambahkan Data+
+                    Tambahkan Data+
             </button>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <form action="<?= BASEURL;?>/mahasiswa/cari" method="post">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Cari Mahasiswa..." name="keyword" id="keyword" aria-label="Recipient's username" aria-describedby="button-addon2" autocomplete="off">
+                    <button class="btn btn-outline-primary" type="submit" id="tombolCari">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
             <h3 class="mt-3">Daftar Mahasiswa</h3>
                 <ul class="list-group">
                 <?php foreach($data['mhs'] as $mhs) :?>
